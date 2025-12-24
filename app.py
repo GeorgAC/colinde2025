fd6a248f5e18d5a899c6dd8c98f2b74f2c10b21a
GeorgAC <91612157+GeorgAC@users.noreply.github.com>
2025-12-24 07:02:07 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="Colinde 2025", page_icon="🎄")
+
+# Link-ul tău de Google Sheets (format CSV)
+SHEET_URL = "docs.google.com"
+
+@st.cache_data(ttl=600)
+def load_data():
+    df = pd.read_csv(SHEET_URL)
+    return df.sort_values(by='Titlu')
+
+st.title("🎶 Colinde de Crăciun")
+
+try:
+    df = load_data()
+    search = st.text_input("Caută colindul:", "")
+    filtered_df = df[df['Titlu'].str.contains(search, case=False, na=False)]
+
+    titlu_ales = st.selectbox("Alege colindul:", filtered_df['Titlu'].unique())
+
+    if titlu_ales:
+        colind = df[df['Titlu'] == titlu_ales].iloc[0]
+        st.header(colind['Titlu'])
+
+        # Media integrată
+        link = str(colind['Link'])
+        if "youtube.com" in link or "youtu.be" in link:
+            st.video(link)
+        elif "dropbox.com" in link:
+            direct_link = link.replace("www.dropbox.com", "dl.dropboxusercontent.com").replace("?dl=0", "")
+            st.audio(direct_link)
+
+        st.subheader("Versuri")
+        st.write(colind['Versuri'])
+except:
+    st.error("Verifică dacă tabelul Google Sheets este public (Anyone with link)!")
